0f8969ec1d54451903a51cd9402b15e37d719a13
undeaD_D <8116188+undeaDD@users.noreply.github.com>
2026-04-07 14:06:47 +0000

    chore: bump kimai-clock to 1.5.4

--- Casks/kimai-clock.rb
+++ Casks/kimai-clock.rb
@@ -1,6 +1,6 @@
 cask "kimai-clock" do
-  version "1.5.3"
-  sha256 "34fc987ddd11db61e31512283904c06b9f686fad08a53d773f4b47891f5c3273"
+  version "1.5.4"
+  sha256 "a0e18b7438fb89f6261cbc02a28e87bf076cb6962b5154b00c74bea4411fdbbd"
   url "https://github.com/Foraum-GmbH/kimai-clock/releases/download/v#{version}/KimaiClock.dmg"
   name "KimaiClock"
   desc "Track and manage Kimai 2 time entries from the menu bar"
